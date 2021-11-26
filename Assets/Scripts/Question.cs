ea2dde653182a6cbf9eb0bfba58e19542eec2d43
Mads Jensen <madsj20@student.sdu.dk>
2021-11-27 00:07:47 +0100

    OnEnter to apply text

---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    [CreateAssetMenu(fileName = "NewQuestion", menuName = "Question")]
+public class Question : ScriptableObject
+{
+    [TextArea(10, 100)]
+    public string question;
+    public string[] answers;
+}
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -9,42 +9,24 @@ public class UIController : MonoBehaviour
 {
     public GameObject UIDocument;
     public string textField;
+    public Question questionRef;
 
     // Start is called before the first frame update
     public void Start()
     {
         var root = UIDocument.GetComponent<UIDocument>().rootVisualElement;
-        
-        /*
-        startButton = root.Q<Button>("start-button");
-        messageButton = root.Q<Button>("message-button");
-        messageText = root.Q<Label>("message-text");
-        startButton.clicked += StartButtonPressed;
-        messageButton.clicked += MessageButtonPressed;
-        */
     }
 
     public void Update()
     {
 
     }
-    /*
-    void StartButtonPressed()
-    {
-        SceneManager.LoadScene("game");
-    }
-    void MessageButtonPressed() {
-       messageText.text = "Subscribe to Coco Code!";
-       messageText.style.display = DisplayStyle.Flex;
-    }
-    */
 
     public void OnEnter()
     {
         var root = UIDocument.GetComponent<UIDocument>().rootVisualElement;
         textField = root.Q<TextField>("AnswerUI").value;
         Debug.Log(textField);
+        root.Q<TextField>("AnswerUI").value = "";
     }
-
-    //public 
 }
